027b0f67e5acfdefb525fa1c2717d35cecc1d16e
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-02 22:57:19 +0400

    fix

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import Checkbox from "components/categoriesChackbox";
-=======
 import Card from "components/card";
->>>>>>> 81a5dbf2ccd2c4c927015e7fce26458ce7a512c7
 import Footer from "containers/footer";
 import Header from "containers/header";
 import { checkboxFakeData } from "helpers/constants";
@@ -14,7 +11,7 @@ export default function Home() {
     <div>
       <Checkbox data={checkboxFakeData} state={state} setState={setState} />
       <Header />
-      <Card/>
+      <Card />
       <Footer />
     </div>
   );
