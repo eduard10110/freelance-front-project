d2cc0afcb7d50b107106b5fc8909768ed9923eb7
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-05 20:50:14 +0400

    fix footer styles

--- src/containers/footer/index.js
+++ src/containers/footer/index.js
@@ -19,68 +19,75 @@ export default function Footer() {
   return (
     <footer className="footer-container-wrapper">
       <div className="footer-subCategories-wrapper">
-        <div className="subCategories-item">
-          <div
-            onClick={handleLogoClick}
-            role="button"
-            className="logo-btn"
-          ></div>
-          <div
-            role="button"
-            onClick={handleDownloadFromAppStore}
-            className="from-app-store shop-logo"
-          ></div>
-          <div
-            role="button"
-            onClick={handleDownloadFromGooglePlay}
-            className="from-google-play shop-logo"
-          ></div>
-          <div
-            role="button"
-            onClick={handleDownloadFromHuaweiStore}
-            className="from-huawei-store shop-logo"
-          ></div>
-        </div>
-        <div className="for-workers-container-wrapper">
-          <p className="body2">Для работодателей</p>
-          <p onClick={handleNavigate(routes.CREATE_TASK)} className="body3">
-            Создать задание
-          </p>
-        </div>
-        <div className="for-workers-container-wrapper">
-          <p className="body2">Для исполнителей</p>
-          <p onClick={handleNavigate(routes.FIND_TASK)} className="body3">
-            Найти задание
-          </p>
-        </div>
-        <div className="for-workers-container-wrapper">
-          <p className="body2">Помощь</p>
-          <p onClick={handleNavigate(routes.CONTACTS)} className="body3">
-            Контакты
-          </p>
-          <p onClick={handleNavigate(routes.SUPPORT)} className="body3">
-            Служба поддержки
-          </p>
-          <p
-            onClick={handleNavigate(routes.POPULAR_QUESTIONS)}
-            className="body3"
-          >
-            Частые вопросы
-          </p>
-          <p className="body3">Как работает Сервис</p>
-          <p className="body2 mt-2">Подписывайся</p>
+        <div className="footer-subCategories">
+          <div className="subCategories-item">
+            <div
+              onClick={handleLogoClick}
+              role="button"
+              className="logo-btn"
+            ></div>
+            <div
+              role="button"
+              onClick={handleDownloadFromAppStore}
+              className="from-app-store shop-logo"
+            ></div>
+            <div
+              role="button"
+              onClick={handleDownloadFromGooglePlay}
+              className="from-google-play shop-logo"
+            ></div>
+            <div
+              role="button"
+              onClick={handleDownloadFromHuaweiStore}
+              className="from-huawei-store shop-logo"
+            ></div>
+          </div>
+          <div className="for-workers-container-wrapper">
+            <p className="body2">Для работодателей</p>
+            <p onClick={handleNavigate(routes.CREATE_TASK)} className="body3">
+              Создать задание
+            </p>
+          </div>
+          <div className="for-workers-container-wrapper">
+            <p className="body2">Для исполнителей</p>
+            <p onClick={handleNavigate(routes.FIND_TASK)} className="body3">
+              Найти задание
+            </p>
+          </div>
+          <div className="for-workers-container-wrapper">
+            <p className="body2">Помощь</p>
+            <p onClick={handleNavigate(routes.CONTACTS)} className="body3">
+              Контакты
+            </p>
+            <p onClick={handleNavigate(routes.SUPPORT)} className="body3">
+              Служба поддержки
+            </p>
+            <p
+              onClick={handleNavigate(routes.POPULAR_QUESTIONS)}
+              className="body3"
+            >
+              Частые вопросы
+            </p>
+            <p className="body3">Как работает Сервис</p>
+            <p className="body2 mt-2">Подписывайся</p>
+          </div>
         </div>
       </div>
-      <div className="footer-bottom footer-subCategories-wrapper">
-        <p className="body3">© 2022 All Rights Reserved</p>
 
-        <div className="bottom-logo">
-          <div className="mir-access-logo"></div>
-          <div className="visa-verification-logo"></div>
-          <div className="mastercard-id-check-logo"></div>
-        </div>
-        <div className="telegram-logo-wrapper">
-          <div className="telegram-logo"></div>
+      <div className="footer-bottom-wrapper">
+        <div className="footer-subCategories-wrapper">
+          <div className="footer-bottom">
+            <p className="body3">© 2022 All Rights Reserved</p>
+
+            <div className="bottom-logo">
+              <div className="mir-access-logo"></div>
+              <div className="visa-verification-logo"></div>
+              <div className="mastercard-id-check-logo"></div>
+            </div>
+            <div className="telegram-logo-wrapper">
+              <div className="telegram-logo"></div>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
