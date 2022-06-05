841b24e1a6b75e056428fa07695cfac9119548eb
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-05 21:27:17 +0400

    fix styles

--- src/containers/homeFirstContainer/index.js
+++ src/containers/homeFirstContainer/index.js
@@ -1,24 +1,35 @@
 import React from "react";
+import girlImage from "assets/images/home-girl.png";
+import boyImage from "assets/images/home-boy.png";
 
 export default function HomeFirstContainer() {
   return (
-    <div className="first-container-wrapper">
-      <div className="container-inner">
-        <h1>melaek</h1>
-        <p className="body1 mt-2">
-          Поможем найти надёжного исполнителя для любых задач
-        </p>
-
-        <div className="buttons-container">
-          <button className="btn btn-purple body7">Заказать услугу</button>
-          <button className="btn btn-purple-filled body7">
-            Стать исполнителем
-          </button>
+    <div className="page-wrapper">
+      <div className="page">
+        <div className="images-wrapper">
+          <img src={girlImage} alt="girl" className="girl-image" />
+          <img src={boyImage} alt="boy" className="boy-image" />
+          {/* <div className="girl-image"></div> */}
         </div>
-      </div>
-      <div className="images-wrapper">
-        <div className="boy-image"></div>
-        <div className="girl-image"></div>
+        <div className="content-wrapper">
+          <div className="first-container-wrapper">
+            <div className="container-inner">
+              <h1>melaek</h1>
+              <p className="body1 mt-2">
+                Поможем найти надёжного исполнителя для любых задач
+              </p>
+
+              <div className="buttons-container">
+                <button className="btn btn-purple body7">
+                  Заказать услугу
+                </button>
+                <button className="btn btn-purple-filled body7">
+                  Стать исполнителем
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/containers/homeReviewsContainer/index.js
+++ src/containers/homeReviewsContainer/index.js
@@ -5,16 +5,22 @@ export default function HomeReviewsContainer() {
   return (
     <div className="reviews-container-wrapper">
       <div className="reviews-container">
-        <h2>Отзывы об исполнителях</h2>
-        <div className="reviews-inner">
-          <ContractCard />
-          <div className="love-with-hands-image-wrapper">
-            <div></div>
+        <div className="content-wrapper">
+          <h2>Отзывы об исполнителях</h2>
+          <div className="reviews-inner">
+            <ContractCard />
+            <div className="love-with-hands-image-wrapper">
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
       <div className="start-today-container-wrapper">
-        <div className="start-today-container"></div>
+        <div className="start-today-container">
+          <div className="content-wrapper">
+            <div className="content"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
