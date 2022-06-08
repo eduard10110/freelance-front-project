1766e1113fe0a0741a7f7178714c009fca2d72d3
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-08 19:49:27 +0400

    add popular questions page

--- src/pages/popularQuestions/index.js
+++ src/pages/popularQuestions/index.js
@@ -1,5 +1,64 @@
-import React from "react";
+import interrogativeGreenImage from "assets/images/interrogative-green.png";
+import interrogativePurpleImage from "assets/images/interrogative-purple.png";
+import openQuestionIcon from "assets/images/open-question-icon.png";
+import closeQuestionIcon from "assets/images/close-question-icon.png";
+import { POPULAR_QUESTIONS_DATA } from "helpers/constants";
+import React, { useState } from "react";
 
 export default function PopularQuestions() {
-  return <div>PopularQuestions</div>;
+  const [openedQuestion, setOpenedQuestion] = useState(null);
+
+  const handleOpen = (id) => () => setOpenedQuestion(id);
+
+  const handleClose = () => setOpenedQuestion(null);
+
+  return (
+    <div className="page-wrapper">
+      <img
+        src={interrogativePurpleImage}
+        alt="interrogativePurple"
+        className="interrogative-purple"
+      />
+      <img
+        src={interrogativeGreenImage}
+        alt="interrogativeGreen"
+        className="interrogative-green"
+      />
+      <div className="page">
+        <div className="content-wrapper popular-questions-container-wrapper">
+          <h2 className="popular-questions-header">Популярные вопросы</h2>
+          <div className="questions-wrapper">
+            <hr />
+            {POPULAR_QUESTIONS_DATA.map(({ label, answer }) => (
+              <div key={label} className="question-item">
+                <div className="question-container">
+                  <h4>{label}</h4>
+                  {openedQuestion === label ? (
+                    <div className="open-close-icon">
+                      <img
+                        onClick={handleClose}
+                        src={closeQuestionIcon}
+                        alt="close"
+                        role="button"
+                      />
+                    </div>
+                  ) : (
+                    <img
+                      onClick={handleOpen(label)}
+                      src={openQuestionIcon}
+                      alt="open"
+                      className="open-close-icon"
+                      role="button"
+                    />
+                  )}
+                </div>
+                {openedQuestion === label && <p className="body3">{answer}</p>}
+                <hr />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
