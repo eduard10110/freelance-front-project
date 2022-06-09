09f43cac56e40c528f7e6aaeeaffd74b02456896
SAHAKYAN <sahakyan@SAHAKYANs-MacBook-Pro.local>
2022-06-09 15:40:51 +0400

    masonary + popular page

--- src/containers/homeCategoriesContainer/index.js
+++ src/containers/homeCategoriesContainer/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { categoriesEnum } from "helpers/enums/categoryEnums";
 import CategoryItem from "components/categoryItem";
-import Masonry from "react-responsive-masonry"
 
 export default function HomeCategoriesContainer() {
+    console.log(categoriesEnum.rightTopColumn, "---------------");
     return (
         <div className="categories-wrapper">
             {/* <h2>Категории</h2> */}
             <div className="caregory-items-wrapper">
                 <div className="caregories-left-column">
-                  <Masonry columnsCount={3} gutter="20px">
                     {categoriesEnum.leftColumn.map((item) => (
                         <CategoryItem
                             width={item.width}
@@ -19,32 +18,57 @@ export default function HomeCategoriesContainer() {
                             key={item.label}
                         />
                     ))}
-                  </Masonry>
                 </div>
-                {/* <div className="caregories-right-column">
+                <div className="caregories-right-column">
                     <div className="categories-right-top">
-                        {categoriesEnum.rightTopColumn.map((item) => (
-                            <CategoryItem
-                                width={item.width}
-                                height={item.height}
-                                label={item.label}
-                                image={item.image}
-                                key={item.label}
-                            />
-                        ))}
+                        <div className="categories-right-top-left">
+                            {categoriesEnum.rightTopColumnLeft.map((item) => (
+                                <CategoryItem
+                                    width={item.width}
+                                    height={item.height}
+                                    label={item.label}
+                                    image={item.image}
+                                    key={item.label}
+                                />
+                            ))}
+                        </div>
+                        <div className="categories-right-top-right">
+                            {categoriesEnum.rightTopColumnRight.map((item) => (
+                                <CategoryItem
+                                    width={item.width}
+                                    height={item.height}
+                                    label={item.label}
+                                    image={item.image}
+                                    key={item.label}
+                                />
+                            ))}
+                        </div>
                     </div>
                     <div className="categories-right-bottom">
-                        {categoriesEnum.rightBottomColumn.map((item) => (
-                            <CategoryItem
-                                width={item.width}
-                                height={item.height}
-                                label={item.label}
-                                image={item.image}
-                                key={item.label}
-                            />
-                        ))}
+                        <div className="categories-right-bottom-top">
+                            {categoriesEnum.rightBottomColumnTop.map((item) => (
+                                <CategoryItem
+                                    width={item.width}
+                                    height={item.height}
+                                    label={item.label}
+                                    image={item.image}
+                                    key={item.label}
+                                />
+                            ))}
+                        </div>
+                        <div className="categories-right-bottom-bottom">
+                            {categoriesEnum.rightBottomColumnBottom.map((item) => (
+                                <CategoryItem
+                                    width={item.width}
+                                    height={item.height}
+                                    label={item.label}
+                                    image={item.image}
+                                    key={item.label}
+                                />
+                            ))}
+                        </div>
                     </div>
-                </div> */}
+                </div>
             </div>
         </div>
     );
--- src/helpers/enums/categoryEnums.js
+++ src/helpers/enums/categoryEnums.js
@@ -15,30 +15,25 @@ import setting from "../../assets/images/categories/setting.png";
 export const categoriesEnum = {
     leftColumn: [
         {label: "Курьерские услуги", width: 295, height: 592, image: delivery},
-        {label: "Разработка по", width: 383, height: 260, image: setting},
-        {label: "Дизайн", width: 316, height: 371, image: ai},
         {label: "Грузоперевозки", width: 397, height: 307, image: dron},
-        {label: "Уборка и помощь по хозяйтсву", width: 351, height: 460, image: cleaning},
-        {label: "Компьютерная помощь", width: 453, height: 344, image: computer},
         {label: "Красота и здоровье", width: 295, height: 460, image: beautyHealth},
-        {label: "Репетиторы и обучение", width: 381, height: 342, image: learning},
-        {label: "Фото, Видео и аудио", width: 453, height: 344, image: photo},
         {label: "Ремонт транспорта", width: 361, height: 344, image: car},
+    ],
+    rightTopColumnLeft: [
+        {label: "Разработка по", width: 383, height: 260, image: setting},
+        {label: "Уборка и помощь по хозяйтсву", width: 351, height: 460, image: cleaning},
+        {label: "Репетиторы и обучение", width: 381, height: 342, image: learning},
+    ],
+    rightTopColumnRight: [
+        {label: "Дизайн", width: 316, height: 371, image: ai},
+        {label: "Компьютерная помощь", width: 453, height: 344, image: computer},
+        {label: "Фото, Видео и аудио", width: 453, height: 344, image: photo},
+    ],
+    rightBottomColumnTop: [
         {label: "Юридическая и бухгалтерская помощь", width: 638, height: 307, image: lower},
+    ],
+    rightBottomColumnBottom: [
         {label: "Ремонт цифровой техники", width: 331, height: 460, image: robot},
         {label: "Мероприятия и промоакции", width: 316, height: 371, image: sale},
     ],
-    // rightBottomColumn: [
-    //     {label: "Юридическая и бухгалтерская помощь", width: 638, height: 307, image: lower},
-    //     {label: "Ремонт цифровой техники", width: 331, height: 460, image: robot},
-    //     {label: "Мероприятия и промоакции", width: 316, height: 371, image: sale},
-    // ],
-    // rightTopColumn: [
-    //     {label: "Разработка по", width: 383, height: 260, image: setting},
-    //     {label: "Уборка и помощь по хозяйтсву", width: 351, height: 460, image: cleaning},
-    //     {label: "Репетиторы и обучение", width: 381, height: 342, image: learning},
-    //     {label: "Дизайн", width: 316, height: 371, image: ai},
-    //     {label: "Компьютерная помощь", width: 453, height: 344, image: computer},
-    //     {label: "Фото, Видео и аудио", width: 453, height: 344, image: photo},
-    // ],
 }
