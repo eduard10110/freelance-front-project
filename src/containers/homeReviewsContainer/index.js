6d0cdcbd159d7c5884dd2c11e00ab8d2cbb74820
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-09 21:19:09 +0400

    add slider

--- src/containers/homeReviewsContainer/index.js
+++ src/containers/homeReviewsContainer/index.js
@@ -1,6 +1,15 @@
+import girlWithComputerImage from "assets/images/girl-with-computer.png";
 import ContractCard from "components/cards/contractCard";
 import React from "react";
-import girlWithComputerImage from "assets/images/girl-with-computer.png";
+import Slider from "react-slick/lib/slider";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
 export default function HomeReviewsContainer() {
   return (
@@ -9,7 +18,14 @@ export default function HomeReviewsContainer() {
         <div className="content-wrapper">
           <h2>Отзывы об исполнителях</h2>
           <div className="reviews-inner">
-            <ContractCard />
+            <div className="slider-wrapper">
+              <Slider {...settings}>
+                <ContractCard />
+                <ContractCard />
+                <ContractCard />
+                <ContractCard />
+              </Slider>
+            </div>
             <div className="love-with-hands-image-wrapper">
               <div></div>
             </div>
