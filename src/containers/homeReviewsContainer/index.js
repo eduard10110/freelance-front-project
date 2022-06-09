7cd0d12396f2083892cd9fd63e66971392ab0304
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-09 20:59:55 +0400

    fix home-reviews container love-with-hands image background

--- src/containers/homeReviewsContainer/index.js
+++ src/containers/homeReviewsContainer/index.js
@@ -3,45 +3,39 @@ import React from "react";
 import girlWithComputerImage from "assets/images/girl-with-computer.png";
 
 export default function HomeReviewsContainer() {
-    return (
-        <div className="reviews-container-wrapper">
-            <div className="reviews-container">
-                <div className="content-wrapper">
-                    <h2>Отзывы об исполнителях</h2>
-                    <div className="reviews-inner">
-                        <ContractCard />
-                        <div className="love-with-hands-image-wrapper">
-                            <div></div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="reviews-container-wrapper">
+      <div className="reviews-container">
+        <div className="content-wrapper">
+          <h2>Отзывы об исполнителях</h2>
+          <div className="reviews-inner">
+            <ContractCard />
+            <div className="love-with-hands-image-wrapper">
+              <div></div>
             </div>
-            <div className="start-today-container-wrapper">
-                <div className="start-content-image">
-                    <img
-                        src={girlWithComputerImage}
-                        // className="girl-with-computer-image"
-                        alt="girl"
-                    />
-                </div>
-              <div className="start-today-container">
-                <div className="helper-div">
-
-                </div>
-                <div className="start-today-content">
-                    <h2>Начните сегодня</h2>
-                    <p className="body1">Исполнители уже готовы вам помочь</p>
-                    <div className="buttons-wrapper">
-                        <button className="btn btn-purple">
-                            Создать задание
-                        </button>
-                        <button className="btn btn-purple-filled">
-                            Найти задание
-                        </button>
-                    </div>
-                </div>
-              </div>
-                {/* <div className="start-today-container">
+          </div>
+        </div>
+      </div>
+      <div className="start-today-container-wrapper">
+        <div className="start-content-image">
+          <img
+            src={girlWithComputerImage}
+            // className="girl-with-computer-image"
+            alt="girl"
+          />
+        </div>
+        <div className="start-today-container">
+          <div className="helper-div"></div>
+          <div className="start-today-content">
+            <h2>Начните сегодня</h2>
+            <p className="body1">Исполнители уже готовы вам помочь</p>
+            <div className="buttons-wrapper">
+              <button className="btn btn-purple">Создать задание</button>
+              <button className="btn btn-purple-filled">Найти задание</button>
+            </div>
+          </div>
+        </div>
+        {/* <div className="start-today-container">
           <div className="content-wrapper">
             <div>
               <img
@@ -64,7 +58,7 @@ export default function HomeReviewsContainer() {
             </div>
           </div>
         </div> */}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
