4ff99d9ee0727514b4e30d2ae07e44e6d5e34c2f
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-10 16:41:04 +0400

    change home background-2 sizes

--- src/pages/selectedTask/index.js
+++ src/pages/selectedTask/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FIND_TASK_MAIN_CONTENT_FAKE_DATA } from "helpers/constants";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -22,6 +23,13 @@ export default function SelectedTask() {
           <div className="selectedTask-header">
             <h3>Установить сантехнику</h3>
             <p className="body4">Задание № 9941868</p>
+          </div>
+          <p className="body1">до 10 000 руб.</p>
+          <div className="task-info-wrapper">
+            <p className="body4">Открыто</p>
+            <p className="body4">1 просмотр</p>
+            <p className="body4">Создано 1 ч. 20 мин. назад</p>
+            <p className="body4">Сантехнические работы</p>
           </div>
         </div>
       </div>
