86f80901d3fa895b8fa1caf5c2427b9550904b7e
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-11 22:40:58 +0400

    fix contacts page styles and support page styles

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -3,35 +3,43 @@ import { YMaps, Map, Placemark } from "react-yandex-maps";
 
 export default function Contacts() {
   return (
-    <div className="contacts-container-wrapper">
-      <div className="contacts-container-location-logo-green"></div>
-      <div className="contacts-container-inner">
-        <div className="contacts-info">
-          <h2>Контакты</h2>
-          <div>
-            <p className="body1">ООО Лекс</p>
-            <p className="body1">ИНН: 6317156167</p>
-            <p className="body1">КПП: 631701001</p>
-            <p className="body1">ОГРН: 1216300056983</p>
-            <p className="body1">Адрес: 443099, Россия, г.</p>
-            <p className="body1">Самара, ул. Чапаевская, д.</p>
-            <p className="body1">85, ком. 9</p>
+    <div className="page-wrapper">
+      <div className="page">
+        <div className="content-wrapper contacts-container-wrapper">
+          <div className="contacts-container-inner">
+            <div className="contacts-info">
+              <h2>Контакты</h2>
+              <div>
+                <p className="body1">ООО Лекс</p>
+                <p className="body1">ИНН: 6317156167</p>
+                <p className="body1">КПП: 631701001</p>
+                <p className="body1">ОГРН: 1216300056983</p>
+                <p className="body1">Адрес: 443099, Россия, г.</p>
+                <p className="body1">Самара, ул. Чапаевская, д.</p>
+                <p className="body1">85, ком. 9</p>
+              </div>
+            </div>
+            <div className="map-container">
+              <YMaps>
+                <Map
+                  width={"520px"}
+                  height={"520px"}
+                  defaultState={{
+                    center: [53.1863672, 50.0914442, 17],
+                    zoom: 14,
+                  }}
+                >
+                  <Placemark
+                    modules={[
+                      "geoObject.addon.balloon",
+                      "geoObject.addon.hint",
+                    ]}
+                    geometry={[53.1863672, 50.0914442, 17]}
+                  />
+                </Map>
+              </YMaps>
+            </div>
           </div>
-        </div>
-        <div className="map-container">
-          <YMaps>
-            <Map
-              width={"21rem"}
-              height={"17rem"}
-              defaultState={{ center: [53.1863672, 50.0914442, 17], zoom: 9 }}
-            >
-              {" "}
-              <Placemark
-                modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
-                geometry={[53.1863672, 50.0914442, 17]}
-              />
-            </Map>
-          </YMaps>
         </div>
       </div>
     </div>
--- src/pages/support/index.js
+++ src/pages/support/index.js
@@ -22,71 +22,75 @@ export default function Support() {
   };
 
   return (
-    <div className="support-container-wrapper">
-      <div className="support-container-header">
-        <div>
-          <h2>Служба поддержки</h2>
-          <h4 className="description">
-            Мы на связи ежедневно 9:00 до 21:00 по московскому времени. Рады
-            помочь и ответить на Ваши вопросы.
-          </h4>
-        </div>
-        <div className="logo"></div>
-      </div>
+    <div className="page-wrapper">
+      <div className="support-logo"></div>
+      <div className="page">
+        <div className="content-wrapper support-container-wrapper">
+          <div className="support-container-header">
+            <div>
+              <h2>Служба поддержки</h2>
+              <h4 className="description">
+                Мы на связи ежедневно 9:00 до 21:00 по московскому времени. Рады
+                помочь и ответить на Ваши вопросы.
+              </h4>
+            </div>
+          </div>
 
-      <div className="inputs-group">
-        <div>
-          <label htmlFor="name" className="body2">
-            Имя
-          </label>
-          <input
-            onChange={handleChange("name")}
-            value={data.name}
-            id="name"
-            className="input"
-            placeholder="Иван"
-          />
-        </div>
-        <div>
-          <label htmlFor="Email" className="body2">
-            Email
-          </label>
-          <input
-            onChange={handleChange("email")}
-            value={data.email}
-            id="Email"
-            className="input"
-            placeholder="Ivanivanov@gmail.com"
-          />
-        </div>
-        <div>
-          <label id="problem-description" className="body2">
-            Опишите проблему
-          </label>
-          <input
-            onChange={handleChange("problemDescription")}
-            value={data.problemDescription}
-            id="problem-description"
-            className="input problem-description"
-            placeholder="Введите текст"
-          />
+          <div className="inputs-group">
+            <div>
+              <label htmlFor="name" className="body2">
+                Имя
+              </label>
+              <input
+                onChange={handleChange("name")}
+                value={data.name}
+                id="name"
+                className="input"
+                placeholder="Иван"
+              />
+            </div>
+            <div>
+              <label htmlFor="Email" className="body2">
+                Email
+              </label>
+              <input
+                onChange={handleChange("email")}
+                value={data.email}
+                id="Email"
+                className="input"
+                placeholder="Ivanivanov@gmail.com"
+              />
+            </div>
+            <div>
+              <label id="problem-description" className="body2">
+                Опишите проблему
+              </label>
+              <textarea
+                onChange={handleChange("problemDescription")}
+                value={data.problemDescription}
+                id="problem-description"
+                className="input problem-description body3"
+                placeholder="Введите текст"
+              />
+            </div>
+          </div>
+          <div className="checkbox-section">
+            <Checkbox
+              label="Нажимая на кнопку, я соглашаюсь на обработку персональных данных"
+              handleCheckboxChange={handleChange}
+              checked={data.acceptPrivacyAndPolicy}
+              id="acceptPrivacyAndPolicy"
+            />
+          </div>
+          <button
+            className={`btn send-button ${
+              checkDisabled() ? "btn-purple-filled" : "btn-filled-disabled"
+            } `}
+          >
+            Отправить
+          </button>
         </div>
       </div>
-      <div className="checkbox-section">
-        <Checkbox
-          label="Нажимая на кнопку, я соглашаюсь на обработку персональных данных"
-          handleCheckboxChange={handleChange}
-          checked={data.acceptPrivacyAndPolicy}
-          id="acceptPrivacyAndPolicy"
-        />
-      </div>
-      <button
-        className={`btn send-button ${
-          checkDisabled() ? "btn-purple-filled" : "btn-filled-disabled"
-        } `}
-      >
-        Отправить
-      </button>
     </div>
   );
 }
