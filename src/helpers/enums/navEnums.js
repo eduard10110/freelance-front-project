2544d90b090e9a6509cf9a0fc3c44f84c9975913
SAHAKYAN <sahakyan@SAHAKYANs-MacBook-Pro.local>
2022-06-19 16:18:00 +0400

    bug fix

--- src/helpers/enums/navEnums.js
+++ src/helpers/enums/navEnums.js
@@ -1,7 +1,7 @@
 import routes from "routes/routes";
 
 export const navLinks = [
-    {label: "Создать задание", path: "/auth"},
+    {label: "Создать задание", path: routes.CREATE_TASK},
     {label: "Найти задание", path: routes.FIND_TASK},
 ];
 
