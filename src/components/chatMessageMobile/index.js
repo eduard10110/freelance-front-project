f2ae3f75b020c01751f999e7113f4f325b1930cd
SAHAKYAN <sahakyan@SAHAKYANs-MacBook-Pro.local>
2022-06-27 22:55:53 +0400

    fix

--- src/components/chatMessageMobile/index.js
+++ src/components/chatMessageMobile/index.js
@@ -1,115 +1,126 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { chatModalVisible } from "store/action-creators/app";
 import chatPreview from "../../assets/images/chat-preview-image.png";
 import Picker, { SKIN_TONE_MEDIUM_DARK } from "emoji-picker-react";
 import { CHAT_MEMBERS_FAKE_DATA } from "../../helpers/constants";
 
-export default function ChatMessageMobile({ selectedMember, setSelectedMember }) {
-  const [emojiVisible, setEmojiVisible] = useState(false);
-  const dispatch = useDispatch();
+export default function ChatMessageMobile({
+    selectedMember,
+    setSelectedMember,
+}) {
+    const [emojiVisible, setEmojiVisible] = useState(false);
 
-  const handleCloseMessage = () => {
-    setSelectedMember(null)
-  };
+    const handleCloseMessage = () => {
+        setSelectedMember(null);
+    };
 
-  const handleEmojiVisible = () => {
-    setEmojiVisible(!emojiVisible);
-  };
+    const handleEmojiVisible = () => {
+        setEmojiVisible(!emojiVisible);
+    };
 
-  const currentMember = CHAT_MEMBERS_FAKE_DATA.find(
-    (data) => data.id === selectedMember
-  );
+    const currentMember = CHAT_MEMBERS_FAKE_DATA.find(
+        (data) => data.id === selectedMember,
+    );
 
-  return (
-    <div className="mobile-chat-message-wrapper">
-      <span
-        className="close-chat-message"
-        onClick={handleCloseMessage}
-        role="presentation"
-      >
-        <i></i>
-      </span>
-      {!selectedMember ? (
-        <div className="chat-preview">
-          <div>
-            <img src={chatPreview} alt="chat" />
-          </div>
-          <h4>Выберите, кому хотели бы написать</h4>
-        </div>
-      ) : (
-        <div className="chat-message-content">
-          {/* CHAT HEAD */}
-          <div className="chat-message-head">
-            <div className="chat-person">
-              {currentMember && (
-                <img
-                  src={require(`../../assets/images/members/${currentMember.image}`)}
-                  alt="avatar"
-                />
-              )}
-              <span>{currentMember && currentMember.name}</span>
-            </div>
-            <div className="person-response">
-              <p className="body3">
-                Пользователь оставил отклик к вашему заданию
-              </p>
-              <p className="body5">«Ремонт мойки из искусственного камня»</p>
-            </div>
-            <div className="person-action">
-              <button className="btn btn-purple-filled body3">
-                Выбрать исполнителем
-              </button>
-            </div>
-          </div>
-          <div className="chat-messages">
-            <div className="self-message">
-              <p className="body3">
-                Здравствуйте ! Хотел бы более подробно узнать о Вашем проекте)
-              </p>
-              <span className="body4">Сегодня, 12:01</span>
-            </div>
-            <div className="incoming-message">
-              <p className="body3">Здравствуйте!</p>
-              <span className="body4">Сегодня, 12:02</span>
-            </div>
-          </div>
-          <div className="mobile-chat-message-footer">
-            <div className="chat-upload">
-              <label htmlFor="upload">
-                <i></i>
-                <input type="file" id="upload" />
-              </label>
-            </div>
-            <div className="chat-emoji" onBlur={() => setEmojiVisible(false)}>
-              <div
-                className="emoji-icon"
-                onClick={handleEmojiVisible}
-                role="presentation"
-              >
-                <i></i>
-              </div>
-              {emojiVisible && (
-                <div className="emoji-content">
-                  <Picker
-                    // onEmojiClick={onEmojiClick}
-                    disableAutoFocus={true}
-                    skinTone={SKIN_TONE_MEDIUM_DARK}
-                    groupNames={{
-                      smileys_people: "PEOPLE",
-                    }}
-                    native
-                  />
+    return (
+        <div className="mobile-chat-message-wrapper">
+            {!selectedMember ? (
+                <div className="chat-preview">
+                    <div>
+                        <img src={chatPreview} alt="chat" />
+                    </div>
+                    <h4>Выберите, кому хотели бы написать</h4>
                 </div>
-              )}
-            </div>
-            <div className="chat-input">
-              <input type="text" className="input" />
-              <i></i>
-            </div>
-          </div>
+            ) : (
+                <div className="mobile-chat-message-content">
+                    {/* CHAT HEAD */}
+                    <div className="mobile-chat-person">
+                            <span
+                                className="close-chat-message"
+                                onClick={handleCloseMessage}
+                                role="presentation"
+                            >
+                                <i></i>
+                            </span>
+                            <div>
+                                {currentMember && (
+                                    <img
+                                        src={require(`../../assets/images/members/${currentMember.image}`)}
+                                        alt="avatar"
+                                    />
+                                )}
+                                <span>
+                                    {currentMember && currentMember.name}
+                                </span>
+                            </div>
+                        </div>
+                    <div className="mobile-chat-message-head">
+                        
+                        <div className="person-response">
+                            <p className="body3">
+                                Пользователь оставил отклик к вашему заданию
+                            </p>
+                            <p className="body5">
+                                «Ремонт мойки из искусственного камня»
+                            </p>
+                        </div>
+                        <div className="person-action">
+                            <button className="btn btn-purple-filled body3">
+                                Выбрать исполнителем
+                            </button>
+                        </div>
+                    </div>
+                    <div className="chat-messages">
+                        <div className="self-message">
+                            <p className="body3">
+                                Здравствуйте ! Хотел бы более подробно узнать о
+                                Вашем проекте)
+                            </p>
+                            <span className="body4">Сегодня, 12:01</span>
+                        </div>
+                        <div className="incoming-message">
+                            <p className="body3">Здравствуйте!</p>
+                            <span className="body4">Сегодня, 12:02</span>
+                        </div>
+                    </div>
+                    <div className="mobile-chat-message-footer">
+                        <div className="chat-upload">
+                            <label htmlFor="upload">
+                                <i></i>
+                                <input type="file" id="upload" />
+                            </label>
+                        </div>
+                        <div
+                            className="chat-emoji"
+                            onBlur={() => setEmojiVisible(false)}
+                        >
+                            <div
+                                className="emoji-icon"
+                                onClick={handleEmojiVisible}
+                                role="presentation"
+                            >
+                                <i></i>
+                            </div>
+                            {emojiVisible && (
+                                <div className="emoji-content">
+                                    <Picker
+                                        // onEmojiClick={onEmojiClick}
+                                        disableAutoFocus={true}
+                                        skinTone={SKIN_TONE_MEDIUM_DARK}
+                                        groupNames={{
+                                            smileys_people: "PEOPLE",
+                                        }}
+                                        native
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        <div className="chat-input">
+                            <input type="text" className="input" />
+                            <i></i>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
