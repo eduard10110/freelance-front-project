fabc67de1391a3e5d312f92193815df4fa0ab363
Eduard10110 <edgabrielyan777@gmail.com>
2022-06-29 21:38:37 +0400

    add unique-performer comments container

--- src/pages/uniquePerformer/mobile/index.js
+++ src/pages/uniquePerformer/mobile/index.js
@@ -1,8 +1,10 @@
-import UniquePerformerPersonalInfoMobile from "components/mobile/unipuePerformerPersonaInfo";
-import React from "react";
-import Slider from "react-slick/lib/slider";
 import workExampleImage1 from "assets/images/my-profile-work-examples-1.png";
 import workExampleImage2 from "assets/images/my-profile-work-examples-2.png";
+import ratingStar from "assets/images/rating-star.png";
+import MyProfileCommentCardMobile from "components/mobile/myProfileCommentCard";
+import UniquePerformerPersonalInfoMobile from "components/mobile/unipuePerformerPersonaInfo";
+import React, { useState } from "react";
+import Slider from "react-slick/lib/slider";
 
 const settings = {
   dots: true,
@@ -13,6 +15,9 @@ const settings = {
 };
 
 export default function UniquePerformer() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleChangeTab = (id) => () => setActiveIndex(id);
   return (
     <div className="page-wrapper">
       <div className="page">
@@ -37,6 +42,50 @@ export default function UniquePerformer() {
               <img src={workExampleImage1} alt="my-profile-work-examples-1" />
               <img src={workExampleImage2} alt="my-profile-work-examples-2" />
             </Slider>
+            <p style={{ marginTop: 80 }} className="body2">
+              Виды выполняемых работ
+            </p>
+            <p className="body3 work-example-category-item btn-td-underline">
+              Ремонт и строительство
+            </p>
+            <p className="body3">Сантехнические работы</p>
+            <p className="body3 work-example-subcategory-lasItem">
+              Электромонтажные работы
+            </p>
+            <div className="star-rating-container-wrapper upm-rating-container-wrapper">
+              <p className="body3">Средняя оценка: 5.0</p>
+              <div className="upm-stars-wrapper">
+                <img src={ratingStar} alt="star" />
+                <img src={ratingStar} alt="star" />
+                <img src={ratingStar} alt="star" />
+                <img src={ratingStar} alt="star" />
+                <img src={ratingStar} alt="star" />
+              </div>
+            </div>
+            <p className="body2">Отзывы</p>
+            <div className="mp-comments-navbar-wrapper">
+              <p className="body3">Сортировать</p>
+              <button
+                onClick={handleChangeTab(0)}
+                className={`btn-default btn-td-underline ${
+                  activeIndex === 0 ? " btn-default-active" : ""
+                }`}
+              >
+                Положительные
+              </button>
+              <button
+                onClick={handleChangeTab(1)}
+                className={`btn-default btn-td-underline ${
+                  activeIndex === 1 ? " btn-default-active" : ""
+                }`}
+              >
+                Отрицательные
+              </button>
+            </div>
+            <MyProfileCommentCardMobile />
+            <MyProfileCommentCardMobile />
+            {/* <MyProfileCommentCard />
+            <MyProfileCommentCard /> */}
           </div>
         </div>
       </div>
